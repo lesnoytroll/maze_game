d7a42f0bc96e48053bfd72cafb01d1ff97bf2087
lesnoytroll <89147640+lesnoytroll@users.noreply.github.com>
2024-03-09 16:24:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,12 @@ class Sprite:
         self.x2 += dx
         self.y1 += dy
         self.y2 += dy
+    def goto(self, x, y):
+        self.x1 = x
+        self.y1 = y
+        width, height = self.img.get_size()
+        self.x2 = x + width
+        self.y2 = y + height
 
     def stands_on(self, target):
         return ((target.x2 > self.x1 > target.x1) or (target.x2 > self.x2 > target.x1)) and (target.y2 > self.y2 > target.y1)
@@ -45,6 +51,7 @@ class Obstacle(Sprite):
 class Buff(Sprite):
     duration: int
     effect: int
+    activated = False
     def __init__(self, img, coords, dur, eff):
         super().__init__(img, coords)
         self.duration = dur
@@ -67,7 +74,7 @@ class Buff(Sprite):
     def deactivate(self, target):
         match self.effect:
             case 1:
-                config.GRAVITY = 0.6
+                config.GRAVITY = 0.8
             case 2:
                 target.speedX /= 1.5
             case 3:
@@ -83,10 +90,13 @@ class Buff(Sprite):
 screen = display.set_mode((config.SCREEN_WIDTH, config.SCREEN_HEIGHT))
 
 player = Player(image.load('king.png'), (config.SCREEN_WIDTH * 0.9, config.SCREEN_HEIGHT * 0.8), 6, 1)
+jump_buff = Buff(image.load('jump_buff.png'), (config.SCREEN_WIDTH * 0.11, config.SCREEN_HEIGHT * 0.86), 5, 1)
+jump_timer = 0
 
 clock = time.Clock()
 game_running = True
 current_level = 1
+
 
 while game_running:
     for ev in event.get():
@@ -112,7 +122,17 @@ while game_running:
             ground3 = Sprite(gr_img, (0, config.SCREEN_HEIGHT * 0.55))
             ground4 = Sprite(gr_img1, (0, 0))
 
-            jump_buff = Buff(image.load('jump_buff.png'), (config.SCREEN_WIDTH * 0.11, config.SCREEN_HEIGHT * 0.86), 5, 1)
+            portal = Sprite(image.load('portal.png'), (config.SCREEN_WIDTH * 0.1, config.SCREEN_HEIGHT * 0.475))
+
+            if jump_buff.activated:
+                jump_buff.goto(-1000, -1000)
+                jump_timer += 1
+                if jump_timer >= config.FPS * jump_buff.duration:
+                    jump_buff.activated = False
+                    jump_buff.deactivate(player)
+                    jump_timer = 0
+            else:
+                jump_buff.goto(config.SCREEN_WIDTH * 0.11, config.SCREEN_HEIGHT * 0.86)
 
             if player.stands_on(ground1) or player.stands_on(ground2) or player.stands_on(ground3):
                 if keys[K_SPACE]:
@@ -124,8 +144,13 @@ while game_running:
             else:
                 player.speedY += config.GRAVITY
 
+            # подбор баффа
             if player.stands_on(jump_buff) or player.head_bump(jump_buff):
                 jump_buff.activate(player)
+                jump_buff.activated = True
+
+            if player.stands_on(portal) or player.head_bump(portal):
+                current_level += 1
 
             screen.fill((15, 15, 15))
             screen.blit(jump_buff.img, (jump_buff.x1, jump_buff.y1))
@@ -133,7 +158,10 @@ while game_running:
             screen.blit(ground2.img, (ground2.x1, ground2.y1))
             screen.blit(ground3.img, (ground3.x1, ground3.y1))
             screen.blit(ground4.img, (ground4.x1, ground4.y1))
+            screen.blit(portal.img, (portal.x1, portal.y1))
+
         case 2:
+            screen.fill((25, 5, 15))
             pass
         case 3:
             pass
